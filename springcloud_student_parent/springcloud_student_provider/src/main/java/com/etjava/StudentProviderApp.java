5385881de5ababcca5c8ea9e463070ee441da843
etjava <etjava@hotmail.com>
2022-10-17 22:00:46 +0800

    服务提供者模块

---
+++ springcloud_student_parent/springcloud_student_provider/src/main/java/com/etjava/StudentProviderApp.java
@@ -0,0 +1,12 @@
+package com.etjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentProviderApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentProviderApp.class, args);
+	}
+}
---
+++ springcloud_student_parent/springcloud_student_provider/src/main/java/com/etjava/controller/StudentController.java
@@ -0,0 +1,69 @@
+package com.etjava.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.etjava.entity.Student;
+import com.etjava.service.StudentService;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+	 @Resource
+	    private StudentService studentService;
+	     
+	    /**
+	     * 添加或者修改学生信息
+	     * @param student
+	     * @return
+	     */
+	    @PostMapping(value="/save")
+	    public boolean save(Student student){
+	        try{
+	            studentService.save(student);  
+	            return true;
+	        }catch(Exception e){
+	            return false;
+	        }
+	    }
+	     
+	    /**
+	     * 查询学生信息
+	     * @return
+	     */
+	    @GetMapping(value="/list")
+	    public List<Student> list(){
+	        return studentService.list();
+	    }
+	     
+	    /**
+	     * 根据id查询学生信息
+	     * @return
+	     */
+	    @GetMapping(value="/get/{id}")
+	    public Student get(@PathVariable("id") Integer id){
+	        return studentService.findById(id);
+	    }
+	     
+	    /**
+	     * 根据id删除学生信息
+	     * @return
+	     */
+	    @GetMapping(value="/delete/{id}")
+	    public boolean delete(@PathVariable("id") Integer id){
+	        try{
+	            studentService.delete(id);
+	            return true;
+	        }catch(Exception e){
+	            return false;
+	        }
+	    }
+}
