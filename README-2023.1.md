b4504de062f48729e3424153705479e8b2ff3c12
etjava <etjava@hotmail.com>
2023-01-01 11:04:44 +0800

    创建公共模块

--- README-2023.1.md
+++ README-2023.1.md
@@ -1,4 +1,27 @@
 # springcloud 完整实例
 # 目录介绍
 springcloud-parent-2023.1 父项目 maven project
-- 
+- common-2023.1 maven module 公共模块 用来存放公共组件 如实体，全局配置，各工具类等
+dependencies
+```
+<dependencies>
+  <dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-data-jpa</artifactId>
+</dependency>
+<dependency>
+    <groupId>mysql</groupId>
+    <artifactId>mysql-connector-java</artifactId>
+</dependency>
+</dependencies>
+<build>
+    <finalName>common-2023.1</finalName>
+    <plugins>
+        <plugin>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-maven-plugin</artifactId>
+        </plugin>
+    </plugins>
+</build>
+```
+
---
+++ springcloud-parent-2023.1/common-2023.1/src/main/java/com/etjava/entity/Teacher.java
@@ -0,0 +1,53 @@
+package com.etjava.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 学生信息实体
+ * @author Administrator
+ *
+ */
+@Entity
+@Table(name="t_teacher")
+public class Teacher implements Serializable{
+ 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+ 
+    @Id
+    @GeneratedValue
+    private Integer id; // 编号
+     
+    @Column(length=50)
+    private String name; // 姓名
+     
+    @Column(length=50)
+    private String subject; // 学科
+     
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+}
