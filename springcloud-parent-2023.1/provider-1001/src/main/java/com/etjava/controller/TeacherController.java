622a4b87a0419d359bc0ad81c308ecdb393abaf2
etjava <etjava@hotmail.com>
2023-01-01 12:47:26 +0800

    调整服务提供者save接收参数

--- springcloud-parent-2023.1/provider-1001/src/main/java/com/etjava/controller/TeacherController.java
+++ springcloud-parent-2023.1/provider-1001/src/main/java/com/etjava/controller/TeacherController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +31,7 @@ public class TeacherController {
 	}
 	
 	@PostMapping("/save")
-	public boolean save(Teacher teacher) {
+	public boolean save(@RequestBody Teacher teacher) {
 		try {
 			teacherService.save(teacher);
 			return true;
